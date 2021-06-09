655742dc71823b95a442aabeef587ffdbed08193
chenxin <823877739@qq.com>
2021-06-09 10:46:55 +0800

    init consul-client

---
+++ consul-client/src/test/java/com/example/consul/client/ConsulClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consul.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
